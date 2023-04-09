5dafbaeec7bac25ba01c35a107729ffe8ef2d288
dod12345 <31848207+dod12345@users.noreply.github.com>
2023-04-09 19:42:20 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.ServiceModel;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ClassLibrary2.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
